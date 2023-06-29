cb91eace94524e29d363ab4fa2dd0e99c26bb70b
Seng Keat <corodostudio@gmail.com>
2023-06-29 17:17:38 +0700

    Create 16.7 Linear Programming Patter.py

---
+++ 16. Linear Programming/16.7 Linear Programming Patter.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def linear_programming(objective_function, constraints):
+  """
+  Solves a linear programming problem.
+
+  Args:
+    objective_function: A list of coefficients for the objective function.
+    constraints: A list of lists of coefficients for the constraints.
+
+  Returns:
+    A list of values for the variables that optimize the objective function.
+  """
+
+  problem = np.zeros((len(objective_function) + len(constraints), len(objective_function)))
+  for i, coefficient in enumerate(objective_function):
+    problem[i, i] = coefficient
+  for i, constraint in enumerate(constraints):
+    for j, coefficient in enumerate(constraint):
+      problem[i + len(objective_function), j] = coefficient
+  for i in range(len(objective_function)):
+    problem[i, -1] = 1
+
+  solution = np.linalg.lstsq(problem, np.ones(len(problem)))[0]
+  return solution[0:len(objective_function)]
+
+
+def main():
+  objective_function = [1, 2]
+  constraints = [[1, 1], [2, 3]]
+
+  solution = linear_programming(objective_function, constraints)
+  print("The solution is:")
+  print(solution)
+
+
+if __name__ == "__main__":
+  main()
+
+# This code first defines a function called linear_programming(). This function takes two arguments: objective_function and constraints. objective_function is a list of coefficients for the objective function. constraints is a list of lists of coefficients for the constraints.
+
+#The linear_programming() function then works by creating a linear programming problem. The linear programming problem is represented by a matrix called problem. The problem matrix has one row for each variable in the problem and one column for each constraint in the problem. The coefficients for the objective function are stored in the first len(objective_function) rows of the problem matrix. The coefficients for the constraints are stored in the remaining rows of the problem matrix.
+
+#The linear_programming() function then solves the linear programming problem using the lstsq() function from the numpy library. The lstsq() function returns a list of values for the variables that optimize the objective function.
+
+#The main() function then calls the linear_programming() function with the objective function and constraints. The main() function then prints the solution that is found by the linear_programming() function.
+
+#To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as linear_programming.py, you can run it by typing the following command into the command line:
